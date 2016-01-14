58ffab903815496df7b7c35a88c61c7779fba95d
Gustavo Falco <comfortablynumb84@gmail.com>
2016-01-14 11:28:42 -0300

    Updated docker_up.sh script.

--- bin/dev/docker_up.sh
+++ bin/dev/docker_up.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-docker run -it -p 1234:80 -v "$PWD":/frenzy php:7-apache /frenzy/bin/dev/docker_helpers/initialize.sh
+CONTAINER_NAME=frenzy;
+
+docker ps -a --filter "name=${CONTAINER_NAME}" | grep frenzy >> /dev/null;
+
+if [ $? -eq "0" ] ; then
+    docker start frenzy && docker exec -ti frenzy bash;
+else
+    docker run --name frenzy -it -p 1234:80 -v "$PWD":/frenzy php:7-apache /frenzy/bin/dev/docker_helpers/initialize.sh;
+fi
